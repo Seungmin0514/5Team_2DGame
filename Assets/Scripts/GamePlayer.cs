318b875e3a505e17eee31f68be971e06fb8f37ab
Seungmin0514 <leessslee88@gmail.com>
2025-10-29 14:15:56 +0900

    feat : DoubleJump

--- Assets/Scripts/GamePlayer.cs
+++ Assets/Scripts/GamePlayer.cs
@@ -1,33 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GamePlayer : MonoBehaviour
 {
-    [SerializeField]protected float speed;
-    [SerializeField]public float Speed {  get; private set; }
-
+    [SerializeField] protected float speed;
     [SerializeField] protected float jumpForce;
-    [SerializeField] public float JumpForce { get; private set; }
-
+    [SerializeField] protected float doublejumpForce;
     [SerializeField] float gravity = 100f;
-
     [SerializeField] Rigidbody2D rigidbody;
-    
+
+    private bool IsGround = false;
+    private bool IsCanDoubleJump = false;
+
 
     private void Update()
     {
         rigidbody.velocity += Vector2.down * gravity * Time.deltaTime;
+
+
+        
     }
+
+
+
     public void PlayerInit(float speed = 10f, float jumpForce = 30f)
     {
         this.speed = speed;
         this.jumpForce = jumpForce;
     }
     
+    
     public void Jump()
     {
-        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+        if (IsGround)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+            IsGround = false;
+        }
+        else if (IsCanDoubleJump)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, doublejumpForce);
+            IsCanDoubleJump = false;
+        }
 
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+                IsGround = true;
+                IsCanDoubleJump=true;
+        }
+    
+    }
+
+
 }
