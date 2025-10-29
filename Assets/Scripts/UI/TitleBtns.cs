18807319d2c446ca402a9a337a8dbe8264af3948
PeacefulKim <corbata@naver.com>
2025-10-29 17:27:23 +0900

    feat: main Scene

--- Assets/Scripts/UI/TitleBtns.cs
+++ Assets/Scripts/UI/TitleBtns.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class TitleBtns : MonoBehaviour
+public class TitleButtons : MonoBehaviour
 {
     public Button playBtn;
     public Button settingBtn;
     public Button quitBtn;
+    public GameObject settingPanel;
 
     private void Awake()
     {
         playBtn.onClick.AddListener(OnClickPlay);
+        settingBtn.onClick.AddListener(OnClickSetting);
         quitBtn.onClick.AddListener(OnClickQuit);
     }
 
     public void OnClickPlay()
     {
         SceneManager.LoadScene("VillageScene");
+    }
+    public void OnClickSetting()
+    {
+        settingPanel.SetActive(true);
     }
     public void OnClickQuit()
     {
