4ac5ab2befea0be011b05b9ab06f684d5eb1e6d1
PeacefulKim <corbata@naver.com>
2025-10-30 10:41:32 +0900

    feat:메인게임: 일시정지 기능 추가

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    public void TogglePause()
+    {
+        if (isPaused) PauseOff();
+        else PauseOn();
+    }
+    public void PauseOn()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        
+        isPaused = true;
+    }
+    public void PauseOff()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+    public void onClickExit()
+    {
+        SceneManager.LoadScene("VillageScene");
+    }
+}
