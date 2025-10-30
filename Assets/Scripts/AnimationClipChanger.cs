3bc0aeb924128dd7f7d7a2a8dea71733df79ec6a
Minji <kuminji125@gmail.com>
2025-10-30 18:58:45 +0900

    feat: 캐릭터 변경
    숫자 1, 2, 3 입력

---
+++ Assets/Scripts/AnimationClipChanger.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationClipChanger : MonoBehaviour
+{
+    private static class OriginalClipNames
+    {
+        public const string IDLE = "Player_Idle";
+        public const string WALK = "PlayerWalk";
+        public const string JUMP = "Player_Jump";
+    }
+    [System.Serializable]
+    public struct PlayerClipSet
+    {
+        public AnimationClip idleClip;
+        public AnimationClip walkClip;
+        public AnimationClip jumpClip;
+    }
+
+    public PlayerClipSet CharacterTypeA;
+    public PlayerClipSet CharacterTypeB;
+    public PlayerClipSet CharacterTypeC;
+
+
+    private Animator animator;
+    private AnimatorOverrideController animatorOverrideController;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }
+
+    public void ChangeClip(string originalClipName, AnimationClip overrideClip)
+    {
+        animatorOverrideController[originalClipName] = overrideClip;
+        Debug.Log($"애니메이션 클립 '{originalClipName}'이 새클립 '{overrideClip}'으로 변경");
+    }
+    // Start is called before the first frame update
+    public void SwitchToSet(PlayerClipSet clipSet)
+    {
+        ChangeClip(OriginalClipNames.IDLE, clipSet.idleClip);
+        ChangeClip(OriginalClipNames.JUMP, clipSet.jumpClip);
+        ChangeClip(OriginalClipNames.WALK, clipSet.walkClip);
+    }
+
+    public void SwitchToTypeA() => SwitchToSet(CharacterTypeA);
+    public void SwitchToTypeB() => SwitchToSet(CharacterTypeB);
+    public void SwitchToTypeC() => SwitchToSet(CharacterTypeC);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)||Input.GetKeyDown(KeyCode.Keypad1)) SwitchToTypeA();
+        if(Input.GetKeyDown(KeyCode.Alpha2)||Input.GetKeyDown(KeyCode.Keypad2)) SwitchToTypeB();
+        if( Input.GetKeyDown(KeyCode.Alpha3)||Input.GetKeyDown(KeyCode.Keypad3)) SwitchToTypeC();
+    }
+}
