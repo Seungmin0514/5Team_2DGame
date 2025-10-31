d5088ed2b3ffc05014883605f51bc0a343491f49
PeacefulKim <corbata@naver.com>
2025-10-31 19:23:55 +0900

    feat: 선택UI 텍스트 추가

--- Assets/Scripts/UI/SelectSpriteLoop.cs
+++ Assets/Scripts/UI/SelectSpriteLoop.cs
@@ -6,9 +6,8 @@ using UnityEngine.UI;
 
 public class SelectSpriteLoop : MonoBehaviour
 {
-    public Animator previewAnimator; // Animator가 붙은 Image 오브젝트
-
-    private RuntimeAnimatorController currentController;
+    public Animator previewAnimator;
+    
 
     public void SetCharacter(CharacterData data)
     {
@@ -17,13 +16,18 @@ public class SelectSpriteLoop : MonoBehaviour
             Debug.LogWarning("Animator Controller가 비어 있습니다: " + data?.name);
             return;
         }
-
-        // 애니메이터 교체
-        previewAnimator.runtimeAnimatorController = data.animatorController;
-        currentController = data.animatorController;
-
-        // 첫 번째 상태부터 재생
-        var firstState = previewAnimator.GetCurrentAnimatorStateInfo(0);
-        previewAnimator.Play(firstState.fullPathHash, 0, 0f);
+        var characterType = data.characterType;
+        switch (characterType)
+        {
+            case CharacterType.One:
+                previewAnimator.SetInteger("CharNum", 0);
+                break;
+            case CharacterType.Two:
+                previewAnimator.SetInteger("CharNum", 1);
+                break;
+            case CharacterType.Three:
+                previewAnimator.SetInteger("CharNum", 2);
+                break;
+        }
     }
 }
